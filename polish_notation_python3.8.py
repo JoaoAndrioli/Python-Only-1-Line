b15035c88198ace220cde904c8fe7c585f2c5191
João Vitor Andrioli de Souza <39911946+JoaoAndrioli@users.noreply.github.com>
2019-11-02 14:27:32 -0300

    Update polish_notation_python3.8.py

--- polish_notation_python3.8.py
+++ polish_notation_python3.8.py
@@ -1,6 +1,6 @@
-pl = lambda *m:[(r:=(r+m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1] #plus
-mi = lambda *m:[(r:=(r-m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1] #minus
-di = lambda *m:[(r:=(r/m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1] #division
-mu = lambda *m:[(r:=(r*m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1] #multiplication
+add = lambda *m:[(r:=(r+m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1]
+sub = lambda *m:[(r:=(r-m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1]
+mul = lambda *m:[(r:=(r/m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1]
+div = lambda *m:[(r:=(r*m[i])) if i!=0 else (r:=m[i]) for i in range(len(m))][-1]
 
-print(pl(4,di(5,2),mi(6,2,mu(10,2,1.6),19),2))
+print(add(2,3,6,3,sub(8,5,mul(2,3,2)),div(3,7)))
